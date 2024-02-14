91e13a541ff9266e39ced719378c2a24c8426028
hemantkumarc <hemantkumarcpersonal@gmail.com>
2024-02-14 18:19:29 +0530

    Added sample C program

---
+++ sample.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int sum(int numOne_, int numTwo_)
+{
+	return numOne_ + numTwo_;
+}
+
+int main(int argc, char* argv[])
+{
+	printf("The first number is : %s\n", argv[1]);
+	printf("The second number is : %s\n", argv[2]);
+
+	int numOne = atoi(argv[1]);
+	int numTwo = atoi(argv[2]);
+
+	printf("The result of the sum of the given numbers is %d\n", sum(numOne, numTwo));
+
+}
